05662ca68ad31db18b09f6a4607adeec17ef1cf7
Rio00o <maplesweeet8@gmail.com>
2024-02-24 11:46:26 +0900

    assignment-4th

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,2 +1,32 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> seasons = new HashMap<>();
+        seasons.put(1, "冬");
+        seasons.put(2, "冬");
+        seasons.put(3, "春");
+        seasons.put(4, "春");
+        seasons.put(5, "春");
+        seasons.put(6, "夏");
+        seasons.put(7, "夏");
+        seasons.put(8, "夏");
+        seasons.put(9, "秋");
+        seasons.put(10, "秋");
+        seasons.put(11, "秋");
+        seasons.put(12, "冬");
+
+        List<String> result = seasons.entrySet().stream()
+                .filter(entry -> entry.getValue().equals("冬"))
+                .map(entry-> entry.getKey() + "月は" + entry.getValue() + "です")
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
